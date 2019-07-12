20c14892d473af926639112d6ecd7078088f6a1a
Rishiber <547091456@qq.com>
2019-07-12 09:53:04 +0800

    发送私信

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -176,5 +176,7 @@ public class UserService implements CommunityConstant {
         return userMapper.updateHeader(userId, headerUrl);
     }
 
-
+    public User findUserByName(String username) {
+        return userMapper.selectByName(username);
+    }
 }
