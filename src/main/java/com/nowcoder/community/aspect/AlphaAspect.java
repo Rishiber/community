2a3e0c988262fa4b55ad549cac7ce5d887547c31
Rishiber <547091456@qq.com>
2019-07-16 10:01:03 +0800

    统一记录日志

---
+++ src/main/java/com/nowcoder/community/aspect/AlphaAspect.java
@@ -0,0 +1,50 @@
+package com.nowcoder.community.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Title: AlphaAspect
+ * @Description:
+ * @Author: Rishiber
+ * @Version: 1.0
+ * @create: 2019/7/12 15:39
+ */
+//@Component
+//@Aspect
+public class AlphaAspect {
+
+    @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before() {
+        System.out.println("before");
+    }
+
+    @After("pointcut()")
+    public void after() {
+        System.out.println("after");
+    }
+
+    @AfterReturning("pointcut()")
+    public void afterReturning() {
+        System.out.println("afterReturning");
+    }
+
+    @AfterThrowing("pointcut()")
+    public void afterThrowing() {
+        System.out.println("afterThrowing");
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("around before");
+        Object obj = joinPoint.proceed();
+        System.out.println("around after");
+        return obj;
+    }
+}
