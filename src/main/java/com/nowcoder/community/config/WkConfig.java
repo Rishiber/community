52a2885ec7206c37338ea2ff0999ae1aba4fadc7
Rishiber <547091456@qq.com>
2019-08-16 20:48:33 +0800

    生成长图

---
+++ src/main/java/com/nowcoder/community/config/WkConfig.java
@@ -0,0 +1,35 @@
+package com.nowcoder.community.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+
+/**
+ * @Title: WkConfig
+ * @Description:
+ * @Author: Rishiber
+ * @Version: 1.0
+ * @create: 2019/8/16 16:53
+ */
+@Configuration
+public class WkConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(WkConfig.class);
+
+    @Value("${wk.image.storage}")
+    private String wkImageStorage;
+
+    @PostConstruct
+    public void init() {
+        // 创建WK图片目录
+        File file = new File(wkImageStorage);
+        if (!file.exists()) {
+            file.mkdir();
+            logger.info("创建WK图片目录：" + wkImageStorage);
+        }
+    }
+}
