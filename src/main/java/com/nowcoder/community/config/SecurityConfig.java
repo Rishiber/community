90d59b3713b704f4bd5e857a6d30ef2f3eaf0531
Rishiber <547091456@qq.com>
2019-08-18 10:20:01 +0800

    项目监控

--- src/main/java/com/nowcoder/community/config/SecurityConfig.java
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -61,7 +61,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements Comm
                 )
                 .antMatchers(
                         "/discuss/delete",
-                        "/data/**"
+                        "/data/**",
+                        "/actuator/**"
                 )
                 .hasAnyAuthority(
                         AUTHORITY_ADMIN
